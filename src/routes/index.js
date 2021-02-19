e1b6816a6678d6b9edd1d1aef2feca25af2fcbe7
cloudapps-idey <77340308+cloudapps-idey@users.noreply.github.com>
2021-02-18 20:29:15 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Cloud Native demo February 19th 2021' });
+  res.render('index', { title: 'Cloud Native demo February 18th 2021' });
 });
 
 module.exports = router;
